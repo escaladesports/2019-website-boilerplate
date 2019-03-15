cf667a47b4d504d2da3b320fc2948aa420dbe107
Kennedy Rose <kennedyianrose@gmail.com>
2019-03-15 15:50:30 -0500

    Split out responsive image component

--- src/components/netlify-image.js
+++ src/components/netlify-image.js
@@ -1,82 +1,36 @@
 import React from 'react'
 import { css } from '@emotion/core'
-import InView from './in-view'
-import Placeholder from './placeholder'
-
-const resizeEvents = []
-let eventListener = false
+import Responsive from './responsive-image'
 
 export default class NetlifyImage extends React.Component{
 	static defaultProps = {
 		resize: `fit`,
 	}
-	constructor(props){
-		super(props)
-		this.state = {
-			w: 0,
-			h: 0,
-		}
-		this.resize = this.resize.bind(this)
-	}
-	componentDidMount(){
-		if(!eventListener){
-			eventListener = true
-			window.addEventListener(`resize`, () => {
-				for(let i = resizeEvents.length; i--;){
-					resizeEvents[i]()
-				}
-			})
-		}
-		resizeEvents.push(this.resize)
-		this.resize()
-	}
-	componentWillUnmount(){
-		resizeEvents.splice(resizeEvents.indexOf(this.resize), 1)
-	}
-	resize() {
-		const {
-			clientWidth: w,
-			clientHeight: h,
-		} = this.container
-		if (w > this.state.w) {
-			this.setState({ w, h })
-		}
-	}
 	render(){
 		const {
-			ratio,
+			src,
 			resize,
+			ratio,
 			width,
 			height,
-			src,
 			...props
 		} = this.props
-		const { w, h } = this.state
 		return (
-			<InView once>
-				{inView => (
-					<div
-						style={{ width }}
-						css={styles.container}
-						ref={el => this.container = el}
-					>
-						<Placeholder ratio={ratio || [width, height]}>
-							{!!w && inView && (
-								<img
-									src={`${src}?nf_resize=${resize}&w=${w}&h=${h}`}
-									{...props}
-								/>
-							)}
-						</Placeholder>
-					</div>
+			<Responsive ratio={ratio} width={width} height={height}>
+				{(w, h) => (
+					<img
+						src={`${src}?nf_resize=${resize}&w=${w}&h=${h}`}
+						css={styles.img}
+						{...props}
+					/>
 				)}
-			</InView>
+			</Responsive>
 		)
 	}
 }
 
 const styles = {
-	container: css`
-		max-width: 100%;
+	img: css`
+		width: 100%;
 	`,
 }
