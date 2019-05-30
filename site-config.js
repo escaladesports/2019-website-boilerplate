06746fea79cbcc0178978812fc7df15a4cbd19fe
Kennedy Rose <kennedyianrose@gmail.com>
2019-05-30 13:38:39 -0500

    Env example

--- site-config.js
+++ site-config.js
@@ -1,7 +1,4 @@
 module.exports = {
-	siteUrl: process.env.URL || `https://project-boilerplate.netlify.com`,
-	siteId: `ecatalog`,
-
 	// For comments Lamda function
 	gitHubRepo: `project-boilerplate`,
 	gitHubOwner: `escaladesports`,
