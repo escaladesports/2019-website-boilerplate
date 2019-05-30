fb291d3dfe53a932e7157c77b7d3ec09ed06e94a
Kennedy Rose <kennedyianrose@gmail.com>
2019-05-30 13:24:44 -0500

    Moved analytics ID

--- site-config.js
+++ site-config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	siteUrl: process.env.URL || `https://project-boilerplate.netlify.com`,
-	siteId: `onix`,
-	analyticsID: `UA-2411855-10`,
+	siteId: `ecatalog`,
 
 	// For comments Lamda function
 	gitHubRepo: `project-boilerplate`,
