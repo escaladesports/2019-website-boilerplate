082c699ce0511e77ba5044a0ee2c102cbe5d6dd8
Kennedy Rose <kennedyianrose@gmail.com>
2019-05-30 14:12:19 -0500

    siteUrl in config

--- site-config.js
+++ site-config.js
@@ -1,4 +1,6 @@
 module.exports = {
+	siteUrl: process.env.URL || `https://project-boilerplate.netlify.com`,
+
 	// For comments Lamda function
 	gitHubRepo: `project-boilerplate`,
 	gitHubOwner: `escaladesports`,
