252db4232fdc6976f2175566b597cf183c85d002
Kennedy Rose <kennedyianrose@gmail.com>
2019-05-30 09:57:20 -0500

    Removed webtasks

--- src/pages/app.js
+++ src/pages/app.js
@@ -3,7 +3,6 @@ import Layout from '../components/layouts/default'
 
 export default class AppPage extends React.Component{
 	render(){
-		console.log(this.props)
 		return(
 			<Layout title='App'>
 				<div>App</div>
