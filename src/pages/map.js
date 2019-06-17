502e10526d24572e4e1adb267f2b87e8be4b8882
Kennedy Rose <kennedy@kennedyrose.com>
2019-06-17 15:14:38 -0400

    Client load map

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { css } from '@emotion/core'
-import { Map } from 'react-leaflet'
 import Layout from '../components/layouts/default'
 import 'leaflet/dist/leaflet.css'
 
 export default class MapPage extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {}
+	}
+	async componentDidMount(){
+		const { Map } = await import(`react-leaflet`)
+		this.setState({ Map })
+	}
 	render(){
+		const { Map } = this.state
 		return(
 			<Layout title='Map'>
 				<h1>Map</h1>
-				<Map
-					center={[51.505, -0.09]}
-					zoom={13} css={styles.map}
-				/>
+				{!!Map && (
+					<Map
+						center={[51.505, -0.09]}
+						zoom={13} css={styles.map}
+					/>
+				)}
 			</Layout>
 		)
 	}
