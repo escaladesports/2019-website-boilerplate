c8de50f5192a76b5a29ef91859f503f2fb917e81
Kennedy Rose <kennedy@kennedyrose.com>
2019-06-17 16:26:50 -0400

    Render pin

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import { css } from '@emotion/core'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
+import iconUrl from 'leaflet/dist/images/marker-icon.png'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
 import Layout from '../components/layouts/default'
-// import 'leaflet/dist/leaflet.css'
+
+// Point icons to images in module
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+	iconRetinaUrl,
+	iconUrl,
+	shadowUrl,
+})
 
 export default class MapPage extends React.Component{
 	constructor(props){
@@ -10,25 +22,36 @@ export default class MapPage extends React.Component{
 	}
 	async componentDidMount(){
 		if (!this.state.Map) {
-			const { Map, TileLayer } = await import(`react-leaflet`)
-			this.setState({ Map, TileLayer })
+			const {
+				Map,
+				TileLayer,
+				Marker,
+				Popup,
+			} = await import(`react-leaflet`)
+			this.setState({ Map, TileLayer, Marker, Popup })
 		}
 	}
 	render(){
-		const { Map, TileLayer } = this.state
+		const { Map, TileLayer, Marker, Popup } = this.state
+		const position = [51.505, -0.09]
 		return(
 			<Layout title='Map'>
 				<h1>Map</h1>
 				{!!Map && (
 					<Map
-						center={[51.505, -0.09]}
-						zoom={1}
+						center={position}
+						zoom={13}
 						css={styles.map}
 					>
 						<TileLayer
 							attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 							url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 						/>
+						<Marker position={position}>
+							<Popup>
+								A pretty CSS3 popup. <br /> Easily customizable.
+							</Popup>
+						</Marker>
 					</Map>
 				)}
 			</Layout>
@@ -38,6 +61,6 @@ export default class MapPage extends React.Component{
 
 const styles = {
 	map: css`
-		height: 600px;
+		height: 400px;
 	`,
 }
