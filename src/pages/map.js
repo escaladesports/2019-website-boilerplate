c33d38d1ebdbca107fb86ba3b7631888732802c8
Kennedy Rose <kennedy@kennedyrose.com>
2019-06-18 14:44:16 -0400

    Locations in markdown

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { css } from '@emotion/core'
-import 'leaflet/dist/leaflet.css'
-import 'react-leaflet-markercluster/dist/styles.min.css'
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
 import iconUrl from 'leaflet/dist/images/marker-icon.png'
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
 import Layout from '../components/layouts/default'
 
+import 'leaflet/dist/leaflet.css'
+import 'react-leaflet-markercluster/dist/styles.min.css'
+
 export default class MapPage extends React.Component{
 	constructor(props){
 		super(props)
 		this.state = {}
+		this.updateLocationsList = this.updateLocationsList.bind(this)
 	}
 	async componentDidMount(){
 		if (!this.state.Map) {
@@ -30,39 +32,68 @@ export default class MapPage extends React.Component{
 				Popup,
 			} = await import(`react-leaflet`)
 
-			const { default: MarkerClusterGroup } = await import(`react-leaflet-markercluster`)
+			const {
+				default: MarkerClusterGroup,
+			} = await import(`react-leaflet-markercluster`)
 
-			this.setState({ Map, TileLayer, Marker, Popup, MarkerClusterGroup })
+			this.setState({
+				Map,
+				TileLayer,
+				Marker,
+				Popup,
+				MarkerClusterGroup,
+			})
 		}
 	}
+	updateLocationsList(){
+		console.log(`updateLocationsList`)
+	}
 	render(){
-		const { Map, TileLayer, Marker, Popup, MarkerClusterGroup } = this.state
-		const position = [51.505, -0.09]
+		const {
+			state: {
+				Map,
+				TileLayer,
+				Marker,
+				Popup,
+				MarkerClusterGroup,
+			},
+			props: {
+				data: {
+					allMarkdownRemark: { edges },
+				},
+			},
+		} = this
+
+		const locations = edges.map(edge => {
+			return edge.node.frontmatter
+		})
+
 		return(
 			<Layout title='Map'>
 				<h1>Map</h1>
 				{!!Map && (
 					<Map
-						center={position}
-						zoom={13}
+						center={[37.993217, -95]}
+						zoom={4}
 						maxZoom={30}
 						css={styles.map}
+						onMoveend={arg => console.log(arg)}
+						ref={this.updateLocationsList}
 					>
 						<TileLayer
 							attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 							url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 						/>
 						<MarkerClusterGroup>
-							<Marker position={position}>
-								<Popup>
-									A pretty CSS3 popup. <br /> Easily customizable.
-								</Popup>
-							</Marker>
-							<Marker position={[51.505, -0.08]}>
-								<Popup>
-									A pretty CSS3 popup. <br /> Easily customizable.
-								</Popup>
-							</Marker>
+							{locations.map((l, key) => (
+								<Marker key={`location${key}`} position={[l.lat, l.lng]}>
+									<Popup>
+										<div><b>{l.title}</b></div>
+										<div>{l.address}</div>
+										<div>{`${l.city}, ${l.state} ${l.zip}`}</div>
+									</Popup>
+								</Marker>
+							))}
 						</MarkerClusterGroup>
 					</Map>
 				)}
@@ -76,3 +107,29 @@ const styles = {
 		height: 400px;
 	`,
 }
+
+export const query = graphql`
+	query MapQuery {
+		allMarkdownRemark(
+			filter: {
+				fileAbsolutePath: {
+					regex: "/src/markdown/locations/"
+				}
+			}
+		){
+			edges{
+				node{
+					frontmatter{
+						title
+						address
+						city
+						state
+						zip
+						lat
+						lng
+					}
+				}
+			}
+		}
+	}
+`
