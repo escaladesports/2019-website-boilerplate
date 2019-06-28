a58e4f8b61dd048ec324a9e185bc59934c8b8a13
Kennedy Rose <kennedyianrose@gmail.com>
2019-06-28 16:47:24 -0500

    Injection test

--- src/components/cms/index.js
+++ src/components/cms/index.js
@@ -1,6 +1,9 @@
+import React from 'react'
 import CMS, { init } from 'netlify-cms'
 import netlifyIdentityWidget from 'netlify-identity-widget'
 import { injectGlobal } from 'emotion'
+import waitForElement from 'wait-for-element'
+import { render } from 'react-dom'
 import { CurrencyControl, CurrencyPreview } from './currency'
 import { HTMLControl, HTMLPreview } from './html'
 
@@ -44,3 +47,18 @@ CMS.registerWidget(`currency`, CurrencyControl, CurrencyPreview)
 CMS.registerWidget(`html`, HTMLControl, HTMLPreview)
 
 init()
+
+!async function injectApp(){
+	const header = await waitForElement(`#nc-root header`, 60 * 60 * 1000)
+	const navUl = header.querySelector(`nav ul`)
+	const li = navUl.querySelector(`li:last-of-type`)
+	console.log(li)
+	const buttonClasses = li.querySelector(`a, button`).className
+
+	const newLi = document.createElement(`li`)
+	navUl.appendChild(newLi)
+	render(
+		<button className={buttonClasses}>Users</button>,
+		newLi,
+	)
+}()
