40d1b654f94ddd29c77a35a374500e57e196ccf0
Kennedy Rose <kennedyianrose@gmail.com>
2019-06-28 14:18:48 -0500

    Promisify management API

--- src/pages/account.js
+++ src/pages/account.js
@@ -6,10 +6,9 @@ import Field from '../components/field'
 import Button from '../components/button'
 import Form from '../components/form'
 import Error from '../components/error-message'
-import Success from '../components/success-message'
 import Loading from '../components/loading'
 import PasswordChange from '../components/accounts/password-change'
-import { login, isAuthenticated, setMetadata, patchUser } from '../utils/auth'
+import { login, isAuthenticated, patchUser } from '../utils/auth'
 import authState from '../state/auth'
 
 export default class AccountPage extends React.Component {
@@ -30,70 +29,50 @@ export default class AccountPage extends React.Component {
 			<Layout title='Your Account'>
 				<h1>Your Account</h1>
 				<Subscribe to={authState}>{
-					({ user, meta, loadingUser, loadingMeta }) => <>
+					({ user, loadingUser }) => <>
 
 						{loadingUser && (
 							<Loading />
 						)}
-						{!loadingUser && (
-							<div>Hi, {meta.name || user.nickname || user.name || `friend`}!</div>
-						)}
-
-						<div>
+						{!loadingUser && <>
+							<div>Hi, {user.name || `friend`}!</div>
 							<h2>Info</h2>
-							{loadingMeta && (
-								<Loading />
-							)}
-							{!loadingMeta && <>
-								<Form
-									onSubmit={async res => {
-										await setMetadata(res)
-									}}
-									recaptcha={false}
-									initialValues={{
-										name: meta.name || ``,
-									}}
-									validationSchema={object().shape({
-										name: string()
-											.required(`required`),
-									})}
-									loading={
-										<Loading />
-									}
-									error={
-										<Error>Server error! Your information was not saved.</Error>
-									}
-									success={
-										<Success>Thank you for your comment! It will be visible once approved.</Success>
-									}
-									form={props => <>
-										<Field
-											label='Name'
-											name='name'
-											{...props}
-										/>
-										<Button
-											type='submit'
-											disabled={props.isSubmitting}
-										>
-											Save
-										</Button>
-									</>}
-								/>
-								<br />
+							<Form
+								onSubmit={async res => {
+									await patchUser(res)
+								}}
+								recaptcha={false}
+								initialValues={{
+									name: user.name || ``,
+								}}
+								validationSchema={object().shape({
+									name: string()
+										.required(`required`),
+								})}
+								loading={
+									<Loading />
+								}
+								error={
+									<Error>Server error! Your information was not saved.</Error>
+								}
+								form={props => <>
+									<Field
+										label='Name'
+										name='name'
+										{...props}
+									/>
+									<Button
+										type='submit'
+										disabled={props.isSubmitting}
+									>
+										Save
+									</Button>
+								</>}
+							/>
+							<br />
+							<PasswordChange />
+						</>}
 
-								<Button
-									type='button'
-									onClick={e => {
-										e.preventDefault()
-										patchUser()
-									}}
-								>
-									Patch
-								</Button>
-								<PasswordChange />
-							</>}
-						</div>
 					</>
 				}</Subscribe>
 			</Layout>
