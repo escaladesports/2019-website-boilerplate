0ada4d4538427ca7d82f6731ee3c6d9c0d6a772b
Kennedy Rose <kennedyianrose@gmail.com>
2019-09-03 11:25:36 -0500

    Removed logs

--- packages/utils/patch-netlify-toml.js
+++ packages/utils/patch-netlify-toml.js
@@ -9,17 +9,12 @@ const dest = join(cwd, `../../netlify.toml`)
 async function go(){
 	const buffer = await readFile(src)
 	let contents = buffer.toString()
-	console.log(`NETLIFY.TOML CONTENTS`, contents)
 	const matches = contents.match(/env\.(.*)/g)
 	matches.forEach(match => {
-		console.log(`MATCH`, match)
 		const parts = match.replace(`env.`, ``).split(/[\W]+/)
 		const key = parts[0]
 		const value = process.env[key] || ``
-		console.log(`TYPEOF CONTENTS`, typeof contents)
-		console.log(`Replacing env.${key} with ${value}`)
 		contents = contents.replace(`env.${key}`, value)
-		console.log(`Succeeded replacing env.${key} with ${value}`)
 	})
 	await outputFile(dest, contents)
 }
